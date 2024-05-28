8ec19cf372639d0ae547460232c8c5742ef68084
Craig Moynes <craig.moynes@gmail.com>
2024-05-28 11:17:03 -0400

    Trying to package into a module

--- webserver.py
+++ webserver.py
@@ -9,88 +9,110 @@ gc.collect()
 import ujson
 import time
 
-try:
-    fp = open('secrets.json')
-    secretsString = fp.read()
-    secrets = ujson.loads(secretsString)
-except OSError as e:
-    print("Error reading secret: ", e)
-    sys.exit()
+class Webserver:
+    s = 0
+    conn = ''
     
-station = network.WLAN(network.STA_IF)
-station.active(True)
-station.connect(secrets['wifi']['ssid'], secrets['wifi']['password'])
-while station.isconnected() == False:
-    print(f"Waiting for connection to wifi...")
-    time.sleep(1)
-    pass
+    def __init(self):
+        try:
+            fp = open('secrets.json')
+            secretsString = fp.read()
+            secrets = ujson.loads(secretsString)
+        except OSError as e:
+            print("Error reading secret: ", e)
+            sys.exit()
+            
+        station = network.WLAN(network.STA_IF)
+        station.active(True)
+        station.connect(secrets['wifi']['ssid'], secrets['wifi']['password'])
+        while station.isconnected() == False:
+            print(f"Waiting for connection to wifi...")
+            time.sleep(1)
+            pass
 
-print(f"Connection successful: {station.ifconfig()}")
+        print(f"Connection successful: {station.ifconfig()}")
 
-try:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    #s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.bind(('',80))
-    s.listen(5)
-except OSError as e:
-    print("Error", e)
-    if s:
-        s.close()
-    raise(e)
+        try:
+            self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            #s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            self.s.bind(('',80))
+            self.s.listen(5)
+        except OSError as e:
+            print("Error", e)
+            if self.s:
+                self.s.close()
+            raise(e)
 
 
-def getFiles():
-    files = []
-    for file in os.listdir():        
-        if file.endswith(".py"):
-            print(f"{file}")
-            files.append(file)
-    files_with_mtime = [(file, os.stat(file)[8]) for file in files]
-    sorted_files = sorted(files_with_mtime, key=lambda x: x[1], reverse=True)
-    for file in sorted_files:
-        print(f"{file[0],file[1]}")
+    async def __getFiles(self):
+        files = []
+        for file in os.listdir():        
+            if file.endswith(".py"):
+                print(f"{file}")
+                files.append(file)
+        files_with_mtime = [(file, os.stat(file)[8]) for file in files]
+        sorted_files = sorted(files_with_mtime, key=lambda x: x[1], reverse=True)
+        for file in sorted_files:
+            print(f"{file[0],file[1]}")
+            
+        ## Keep only last 5 files
+        for file in sorted_files[3-len(sorted_files):len(sorted_files)]:
+            print(f"Removing {file[0]}")
+            #os.remove(file[0])
+        return [file[0] for file in sorted_files]
+    def __getPage(self):
+        files = self.__getFiles()
+        print(f"{files}")
         
-    ## Keep only last 5 files
-    for file in sorted_files[3-len(sorted_files):len(sorted_files)]:
-        print(f"Removing {file[0]}")
-        #os.remove(file[0])
-    return [file[0] for file in sorted_files]
-def getPage():
-    files = getFiles()
-    print(f"{files}")
-    
-    html = """
-        <html>
-        <head><title>Elliptical Data</title><head>
-    """
-    for file in files:
-        html += "<a href=" + file + " download>" + file + "</a>"
-        html += "<br/>"
-    html +="""
-        </html>
-    """
-    return html
+        html = """
+            <html>
+            <head><title>Elliptical Data</title><head>
+        """
+        for file in files:
+            html += "<a href=" + file + " download>" + file + "</a>"
+            html += "<br/>"
+        html +="""
+            </html>
+        """
+        return html
 
-conn = ''
+    def server(self):
+        
+        self.__init()
+        
+        
+        try:
+            while True:
+                print(f"Waiting for connection...")
+                self.conn, addr = self.s.accept()
+                print(f"Got a connection from {str(addr)}")
+                request = conn.recv(1024)
+                request = str(request)
+                print(f"Content: {request}")
+                response = self.__getPage()
+                self.conn.send("HTTP/1.1 200 OK\n")
+                self.conn.send("Content-Type: text/html\n")
+                self.conn.send("Connection: close\n\n")
+                self.conn.sendall(response)
+                self.conn.close()
+                self.__close()
+        except OSError as e:
+            print("OSError: ", e)
+            self.__close()
+        except KeyboardInterrupt:
+            self.__close()
+            
+    def __close(self):
+        if self.conn:
+            self.conn.close()
+            if self.s:
+                self.s.close()
+            
 try:
-    while True:
-        print(f"Waiting for connection...")
-        conn, addr = s.accept()
-        print(f"Got a connection from {str(addr)}")
-        request = conn.recv(1024)
-        request = str(request)
-        print(f"Content: {request}")
-        response = getPage()
-        conn.send("HTTP/1.1 200 OK\n")
-        conn.send("Content-Type: text/html\n")
-        conn.send("Connection: close\n\n")
-        conn.sendall(response)
-        conn.close()
+    w = Webserver()
+    w.server()
+    print("Done?")
+    #while True:
+    #    pass
 except OSError as e:
-    print("OSError: ", e)
-    s.close()
-except KeyboardInterrupt:
-    if conn:
-        conn.close()
-    if s:
-        s.close()
+    print("Error ", e)
